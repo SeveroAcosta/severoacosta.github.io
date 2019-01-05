8c153ed0ca39763a324e4d9364721f0083a973b2
SeveroAcosta <46407838+SeveroAcosta@users.noreply.github.com>
2019-01-05 22:22:52 +0000

    Add files via upload

---
+++ assets/photos.ts
@@ -0,0 +1,5 @@
+export const expos: { name: string, amountOfPhotos: number, format: string }[] = [
+        { "name": "expo1", "amountOfPhotos": 5, "format": ".jpeg" },
+        { "name": "expo2", "amountOfPhotos": 20, "format": ".jpg" }
+];
+  
